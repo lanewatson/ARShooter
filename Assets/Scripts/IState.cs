7842912b0070df9260b22bab1318c9f343d89c7b
Lane Watson <44728749+lanewatson@users.noreply.github.com>
2021-08-20 16:37:05 -0400

    Created StateMachine script

--- Assets/Scripts/IState.cs
+++ Assets/Scripts/IState.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IState : MonoBehaviour
+public interface IState
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
